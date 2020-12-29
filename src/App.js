b1f8d62377208d30749e8d6c149d928712550d66
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2020-12-29 16:21:53 +0900

    [change]寺子屋29

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3> Code </h3>
+      <h2 className="title">JavaScript 最強伝説</h2>
+      <p className="text">
+        鬱蒼と木々が茂った森の中に、金色に輝く王笏がある。
+        一番上には金剛石が付いていて、持つもの威厳を示すような、むしろ彼の威厳こそが内側で乱反射しているかのような、権威の構造を象徴してそこに佇んでいた。
+      </p>
+      <p className="text2">しかし未だ、誰もその王笏を見たものはない。</p>
+      <p className="text">
+        森の伝説では、これを手にした者は JavaScript
+        王としての責務を授かるのだという。
+      </p>
+      <p className="text2">特にこれを探し求めている者もいないようだが。</p>
     </div>
   );
 }
