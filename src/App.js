fffbe61a5954177a7feb65b8d85bc0898af39e98
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2020-12-29 16:28:07 +0900

    [change]寺子屋30

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "./styles.css";
 
+// React プロジェクトを開く
+// 変数を宣言する
+// JSX の中の {} の中で変数を呼び出して
+// 文章を表示する
+const message = "これがmessageです！";
+
 export default function App() {
-  return (
-    <div className="App">
-      <h2 className="title">JavaScript 最強伝説</h2>
-      <p className="text">
-        鬱蒼と木々が茂った森の中に、金色に輝く王笏がある。
-        一番上には金剛石が付いていて、持つもの威厳を示すような、むしろ彼の威厳こそが内側で乱反射しているかのような、権威の構造を象徴してそこに佇んでいた。
-      </p>
-      <p className="text2">しかし未だ、誰もその王笏を見たものはない。</p>
-      <p className="text">
-        森の伝説では、これを手にした者は JavaScript
-        王としての責務を授かるのだという。
-      </p>
-      <p className="text2">特にこれを探し求めている者もいないようだが。</p>
-    </div>
-  );
+  return <div className="App">{message}</div>;
 }
