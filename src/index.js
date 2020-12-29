8493e84e31216d06861d189e6436908d3d78d056
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2020-12-29 16:19:41 +0900

    [change]寺子屋28

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+//StrictModeは古い記法などのチェックを行う
 ReactDOM.render(
   <React.StrictMode>
     <App />
