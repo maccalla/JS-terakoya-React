78f5699d83a328d7b6c092a9630c06fec12e4c83
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2020-12-29 16:32:46 +0900

    [change]寺子屋31

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ import "./styles.css";
 // 変数を宣言する
 // JSX の中の {} の中で変数を呼び出して
 // 文章を表示する
-const message = "これがmessageです！";
+const message = "犬";
+
+// switch 用いて、返す値が分岐する関数を実装する
+// 上記の関数と同じ内容になる if を使った関数を実装する
+const result = (message) => {
+  switch (message) {
+    case "犬":
+      return "コンテキストは犬";
+    case "猫":
+      return "コンテキストは猫";
+    default:
+      return "x";
+  }
+};
 
 export default function App() {
-  return <div className="App">{message}</div>;
+  return (
+    <div className="App">
+      <dev>{message}</dev>
+      <br />
+      <dev>{result(message)}</dev>
+    </div>
+  );
 }
